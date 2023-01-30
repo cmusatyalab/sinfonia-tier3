326575920e51addeb3c0ae98623717c3dd5b3d0a
Jan Harkes <jaharkes@cs.cmu.edu>
2023-01-30 10:35:13 -0500

    Add option to generate qrcode config for wireguard-android

--- src/sinfonia_tier3/cli.py
+++ src/sinfonia_tier3/cli.py
@@ -14,6 +14,7 @@ import argparse
 from typing import Sequence
 from uuid import UUID
 
+import pyqrcode
 from requests.exceptions import HTTPError
 from yarl import URL
 
@@ -46,6 +47,9 @@ def parse_args(args: list[str] | None = None) -> argparse.Namespace:
         "--version", action="version", version=f"%(prog)s {__version__}"
     )
     parser.add_argument(
+        "--qrcode", action="store_true", help="Generate QR code for wireguard-android"
+    )
+    parser.add_argument(
         "--zeroconf",
         action="store_true",
         help="Try to discover local Tier2 through MDNS",
@@ -62,6 +66,7 @@ def sinfonia_tier3(
     application: Sequence[str],
     config_debug: bool = False,
     debug: bool = False,
+    qrcode: bool = False,
     zeroconf: bool = False,
 ) -> int:
     # Request one or more backend deployments
@@ -79,6 +84,12 @@ def sinfonia_tier3(
     # Pick the best deployment (first returned for now...)
     deployment_data = deployments[0]
 
+    if qrcode:
+        wgconf = deployment_data.tunnel_config.to_wgconfig(wgquick_format=True)
+        qrconf = pyqrcode.create(wgconf, error="L")
+        print(qrconf.terminal(quiet_zone=1))
+        return 0
+
     return sinfonia_runapp(
         deployment_data.deployment_name,
         deployment_data.tunnel_config,
@@ -95,5 +106,6 @@ def main() -> int:
         args.application,
         config_debug=args.config_debug,
         debug=args.debug,
+        qrcode=args.qrcode,
         zeroconf=args.zeroconf,
     )
